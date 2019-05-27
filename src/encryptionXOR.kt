a24c2ebbb8a35ea08b3ab145d815f30d77a29dd2
Georgix <KarelinOlegSPB@yandex.ru>
2019-05-27 13:32:10 +0300

    1

--- src/encryptionXOR.kt
+++ src/encryptionXOR.kt
@@ -14,30 +14,35 @@ package src
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
-//Вынести чтение файла в отдельный блок
+import java.lang.StringBuilder
+
+//Вынести чтение файла в отдельный блок+
 //Перепроверить систему шифрования битов
-//stringBuilder к строке
+//stringBuilder к строке+
 //Генератор случайного текста для тестов
 //
 // Task 3
 // Двумерная карта с клетками, лабиринт, перемещение, взаимодействие с клетками, найти набор вещей - вытащить.
 // Генерирует лабиринт, ивенты, квик тайм ивенты,
-class EncryptionXOR{
-    fun decryption(key: String, outputText: String, inputText: String) {
-        File(inputText).delete()
-        File(inputText).createNewFile()
-        val msg1 = File(outputText).readText()
-        var msg = ""
-        for (line in msg1) msg += line
-        val output = File(inputText)
-        val writer = BufferedWriter(FileWriter(output, true))
-        var hexToPairs = ""
+//            val charset = Charsets.UTF_8
+//            val byteArray = msg.toByteArray(charset)
+//            byteArray.forEach { hexToPairs += it}
+
+class EncryptionXOR(tempKey: String, inputText: String ,outputText: String){
+    private val inputTextTemp = File(inputText).readText()
+    private val outputFile = File(outputText)
+    private val writer = BufferedWriter(FileWriter(outputFile, true))
+    private val key = tempKey
+    private var msg = ""
+    fun decryption() {
+        for (line in inputTextTemp) msg += line
+        var hexToPairs = StringBuilder("")
         var i = 0
         while (i < msg.length) {
             //Разделяем по нескольким парам(xor операция)
             val new = msg.substring(i, i + 2)
             val decimal = new.toInt(16)
-            hexToPairs += decimal.toChar()
+            hexToPairs.append(decimal.toChar())
             i += 2
         }
         var keyItr = 0
@@ -48,24 +53,15 @@ class EncryptionXOR{
             if (keyItr >= key.length) {
                 keyItr = 0
             }
-
         }
         writer.close()
     }
-    fun encryption(key: String, msg: String, output: String) {
-        File(output).delete()
-        File(output).createNewFile()
-        val inputTextTemp = File(msg).readText()
-        val outputFile = File(output)
-        var inputText = ""
-        for (line in inputTextTemp) {
-            inputText += line
-        }
-        val writer = BufferedWriter(FileWriter(outputFile, true))
+    fun encryption() {
+        for (line in inputTextTemp) msg += line
         var keyItr = 0
-        for (i in 0 until inputText.length) {
+        for (i in 0 until msg.length) {
             //Разделяем сетку по нескольким парам(xor операция)
-            val temp = inputText[i].toInt() xor key[keyItr].toInt()
+            val temp = msg[i].toInt() xor key[keyItr].toInt()
             writer.write(String.format("%02x", temp.toByte()))
             keyItr++
             if (keyItr >= key.length) {
@@ -78,16 +74,14 @@ class EncryptionXOR{
 fun main(args: Array<String>) {
     if (args[0] ==  "-d")  {
         if (args[3] != "-o") {
-            File(args[3]).createNewFile()
-            return EncryptionXOR().decryption(args[1], args[2], args[3])
+            return EncryptionXOR(args[1], args[2], args[3]).decryption()
         }
-        return EncryptionXOR().decryption(args [1],args [2],args[4])
+        File(args[4]).forEachLine { println (it) }
     }
     if (args[0] ==  "-c")  {
         if (args[3] != "-o") {
-            File(args[3]).createNewFile()
-            return EncryptionXOR().encryption(args[1], args[2], args[3])
+            return EncryptionXOR(args[1], args[2], args[3]).encryption()
         }
-        return EncryptionXOR().encryption(args [1],args [2],args[4])
+        File(args[4]).forEachLine { println (it) }
     }
 }
--- src/test.kt
+++ src/test.kt
@@ -2,17 +2,38 @@ package src
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.io.File
 import java.lang.Exception
 import java.lang.IllegalArgumentException
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import java.util.Random
 
+//const val length = 10;
+//const val charPool = "[a-zA-Z0-9]+"
+//val randomString = (1..length)
+//        .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+//        .map(charPool::get)
+//        .joinToString("")
 class Test {
     @Test
     fun encryptor(){
-        EncryptionXOR().encryption("123", "src/res/inputText.txt", "src/res/outputText.txt")
+        EncryptionXOR("123", "src/res/inputText.txt", "src/res/outputText.txt").encryption()
     }
     @Test
     fun decryptor(){
-        EncryptionXOR().decryption("123", "src/res/outputText.txt", "src/res/inputText.txt")
+        EncryptionXOR("123", "src/res/outputText.txt", "src/res/inputText.txt").decryption()
+    }
+    @Test
+    fun check() {
+        val startText = File("src/res/inputText.txt").readText()
+        EncryptionXOR("123", "src/res/inputText.txt", "src/res/outputText.txt").encryption()
+        File("src/res/inputText.txt").delete()
+        File("src/res/inputText.txt").createNewFile()
+        EncryptionXOR("123", "src/res/outputText.txt", "src/res/inputText.txt").decryption()
+        File("src/res/outputText.txt").delete()
+        File("src/res/outputText.txt").createNewFile()
+        val decText = File("src/res/inputText.txt").readText()
+        assertEquals(decText, startText)
     }
 }
